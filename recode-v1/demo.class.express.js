77561a3f3f81f1a8fd942cecaaed48157e36075c
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-06-08 16:00:45 +0700

    Pcommit change

--- recode-v1/demo.class.express.js
+++ recode-v1/demo.class.express.js
@@ -4,41 +4,36 @@ class Express {
       params: {},
       query: {},
       body: {
-        name: "thiengg",
-      },
+        name: 'thiengg'
+      }
     },
     res = {},
     next = {}
   ) {
-    this.request = req;
-    this.response = res;
-    this.nextFunc = next;
+    this.request = req
+    this.response = res
+    this.nextFunc = next
   }
 
   use(callback) {
     try {
-      if (this.request.body.name === "thieng") {
-        callback(this.request, this.response, this.nextFunc);
+      if (this.request.body.name === 'thieng') {
+        callback(this.request, this.response, this.nextFunc)
       } else {
-        callback(
-          { error: "error" },
-          this.request,
-          this.response,
-          this.nextFunc
-        );
+        callback({ error: 'error' }, this.request, this.response, this.nextFunc)
       }
     } catch (error) {
-      callback(error, this.request, this.response, this.nextFunc);
+      callback(error, this.request, this.response, this.nextFunc)
     }
   }
 }
 
-const app = new Express();
+const app = new Express()
 app.use((req, res, next) => {
-  console.log(req.body);
-});
+  console.log(req.body)
+})
 
 app.use((error, req, res, next) => {
-  console.log("error", error);
-  console.log(req.body);
-});
+  console.log('error', error)
+  console.log(req.body)
+})
